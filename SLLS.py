b31264e5c476efd59a390fb7abd4022ae241a897
Nimish Sakalkale <141994765+nsakalkale@users.noreply.github.com>
2024-03-21 01:11:14 +0530

    Update SLLS.py

--- SLLS.py
+++ SLLS.py
@@ -1,6 +1,6 @@
 import os
 import streamlit as st
-from langchain.llms import OpenAI
+from langchain_community.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SimpleSequentialChain
 
